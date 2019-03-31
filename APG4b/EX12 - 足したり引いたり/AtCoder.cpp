6bda7cfc2b629f3534d5e8ee144b7e9617b97724
Tsune <48820371+bit-san@users.noreply.github.com>
2019-03-31 18:10:50 +0900

    Add files via upload

---
+++ APG4b/EX12 - 足したり引いたり/AtCoder.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "bits_stdc++.h"
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+
+	// 計算結果
+	int count = 1;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s.at(i) == '+'){
+			count ++;
+		}
+		else if (s.at(i) == '-') {
+			count --;
+		}
+	}
+	cout << count << endl;
+}

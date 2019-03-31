b72cf68c76b9b2e59d347bf0fd7198c2b2cf3d1f
Tsune <48820371+bit-san@users.noreply.github.com>
2019-03-31 19:01:34 +0900

    Add files via upload

---
+++ APG4b/EX13 - 平均との差/AtCoder.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "bits_stdc++.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int N;
+	int sum = 0;	// 合計点
+
+	cin >> N;
+	// N要素の配列
+	vector<int> vec(N);
+	// 入力処理
+	for (int i = 0; i < N; i++) {
+		cin >> vec.at(i);
+	}
+	// 合計点を計算
+	for (int i = 0; i < N; i++) {
+		sum = sum + vec.at(i);
+	}
+	cout << "合計:" << sum << endl;
+
+	
+	// 平均点
+	int mean = sum / N;
+	// 平均点から何点離れているかを計算して出力
+	for (int i = 0; i < N; i++) {
+
+		if (vec.at(i) < mean) {
+			vec.at(i) = mean - vec.at(i);
+		}
+		else {
+			vec.at(i) = vec.at(i) - mean;
+		}
+		cout << i+1 << ":" << vec.at(i) << endl;
+	}
+}

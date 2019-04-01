e1222dd44f1d605ea5c6d068b4faf0dce6f01bff
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-01 21:27:59 +0900

    Add files via upload

---
+++ APG4b/EX14 - 三人兄弟の身長差/AtCoder.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int A, B, C,max,min;
+	cin >> A >> B >> C;
+	vector<int> vec = { A,B,C };
+	sort(vec.begin(), vec.end());
+	min = vec.at(0);
+	reverse(vec.begin(), vec.end());
+	max = vec.at(0);
+	cout << max - min <<endl;
+}

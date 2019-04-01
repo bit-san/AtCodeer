a051bb871415a9c20ad2be4df9ae5329457eee38
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-01 13:21:52 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC087B - Coins/AtCoder.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "bits_stdc++.h"
+
+using namespace std;
+
+int main(){
+	int A, B, C, X;
+	int count = 0;
+	int total;
+
+	/*	Aの入力 => 0からAまで
+		Bの入力 => 0からCまで
+		Cの入力 => 0からCまで	*/
+	cin >> A >> B >> C >> X;
+	
+	/*	組み合わせ
+		A=0			 : B=0			: C=0からCまで  =>  total==Xならカウント
+		A=0			 : B=0からBまで : C=0からCまで  =>  total==Xカウント
+		A=0からAまで : B=0からBまで : C=0からCまで  =>  total==Xカウント	*/
+	for (int x = 0; x <= A; x++) {
+		for (int y = 0; y <= B; y++) {
+			for (int z = 0; z <= C; z++) {
+				total = 500 * x + 100 * y + 50 * z; // 合計金額
+				if (total == X) {
+					count++; // 合計金額とXが同じならカウント
+				}
+			}
+		}
+	}
+	cout << count << endl;
+}
---
+++ AtCoder Beginners Selection/ABC087B - Coins/AtCoder2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "bits_stdc++.h"
+
+using namespace std;
+
+// #defineで同じ処理を一つにまとまる
+#define r(i,n) for(int i=0;i<=n;i++)
+int a, b, c, r, x;
+int main() { 
+	std::cin >> a >> b >> c >> x;
+	r(i, a) {
+		r(j, b) {
+			r(k, c) {
+				//合計 != Xですか ? true:false
+				i * 10 + j * 2 + k != x / 50 ? r:r++;
+			}
+		}
+	}
+	std::cout << r; 
+}

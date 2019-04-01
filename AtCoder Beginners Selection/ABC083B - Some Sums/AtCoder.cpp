8d09dc7c101a4fec1a3309b59a6939f91a27b90d
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-01 23:13:00 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC083B - Some Sums/AtCoder.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	int sum = 0;
+	for (int i = 0; i <= n; i++) {
+		int x = i % 10 + i / 10 % 10 + i / 100 % 10 + i / 1000 % 10 + i / 10000 % 10;
+		if (x >= a && x <= b) {
+			sum += i;
+		}
+	}
+	cout << sum << endl;
+}

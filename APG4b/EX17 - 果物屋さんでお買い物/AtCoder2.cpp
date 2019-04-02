17db8049fc1764c2ebbdcbc8bbee4097c74b59f0
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-02 17:57:05 +0900

    Add files via upload

---
+++ APG4b/EX17 - 果物屋さんでお買い物/AtCoder2.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+#define r(i,n) for(int i=0;i<n;i++)
+using namespace std;
+
+int main() {
+	int N, S,count=0;
+	cin >> N >> S;
+	vector<int> A(N), P(N);
+	r(i,N){cin >> A.at(i);}
+	r(i,N){cin >> P.at(i);}
+
+	// リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
+	// ここにプログラムを追記
+	r(i,N)r(j,N) if(S == A.at(i)+P.at(j)){count++;}
+	cout << count << endl;
+}

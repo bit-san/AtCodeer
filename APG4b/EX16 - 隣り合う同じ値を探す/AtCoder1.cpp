fb9cdb1287f6fb0321b4f744913bca0f1581815e
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-02 17:17:53 +0900

    Add files via upload

---
+++ APG4b/EX16 - 隣り合う同じ値を探す/AtCoder1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	vector<int> data(5);
+	bool handan=false;
+	for (int i = 0; i < 5; i++) {
+		cin >> data.at(i);
+	}
+
+	if (data.at(0) == data.at(1)) {
+		handan = true;
+	}
+	if (data.at(1) == data.at(2)) {
+		handan = true;
+	}
+	if (data.at(2) == data.at(3)) {
+		handan = true;
+	}
+	if (data.at(3) == data.at(4)) {
+		handan = true;
+	}
+	if (handan == true) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}
---
+++ APG4b/EX16 - 隣り合う同じ値を探す/AtCoder2.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	vector<int> data(5);
+	bool handan=false;
+	for (int i = 0; i < 5; i++) {
+		cin >> data.at(i);
+	}
+	//0と1、1と2、2と3、3と4　よって0<i<4
+	for (int i = 0; i < 4; i++) {
+		if (data.at(i) == data.at(i+1)) {
+			handan = true;
+		}
+	}
+	
+	if (handan == true) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+	return 0;
+}

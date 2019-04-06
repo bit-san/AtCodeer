8eb3b42eba98d1c729bd5e1b68a5585a4aa781d5
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-06 20:01:47 +0900

    Add files via upload

---
+++ APG4b/EX20 - 報告書の枚数/AtCoder.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int count_report_num(vector<vector<int>> &children, int x) {
+	if (children.at(x).size() == 0) {
+		return 1;
+	}
+	int sum = 0;
+	for (int i = 0; i < children.at(x).size(); i++) {
+		int c = children.at(x).at(i);
+		sum += count_report_num(children, c);
+	}
+	sum += 1;
+	return sum;
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<int> p(N);
+	p.at(0) = -1;
+	for (int i = 1; i < N; i++) {
+		cin >> p.at(i);
+	}
+
+	vector<vector<int>> children(N);
+	for (int i = 1; i < N; i++) {
+		int parent = p.at(i);
+		children.at(parent).push_back(i);
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << count_report_num(children, i) << endl;
+	}
+}

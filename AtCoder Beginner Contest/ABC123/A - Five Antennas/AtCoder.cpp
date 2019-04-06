b9cdc64267b020b85fc9891b9be8beaa36e0bd85
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-06 23:42:51 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC123/A - Five Antennas/AtCoder.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int a, b, c, d, e, k;
+	bool flg = false;
+	cin >> a >> b >> c >> d >> e >> k;
+
+	if (b - a > k) {
+		flg = true;
+	}
+	if (c - a > k) {
+		flg = true;
+	}
+	if (d - a > k) {
+		flg = true;
+	}
+	if (e - a > k) {
+		flg = true;
+	}
+
+	if (c - b > k) {
+		flg = true;
+	}
+	if (d - b > k) {
+		flg = true;
+	}
+	if (e - b > k) {
+		flg = true;
+	}
+
+	if (d - c > k) {
+		flg = true;
+	}
+	if (e - c > k) {
+		flg = true;
+	}
+
+	if (e - d > k) {
+		flg = true;
+	}
+
+	if (flg==false) {
+		cout << "Yay!" << endl;
+	}
+	else {
+		cout << ":(" << endl;
+	}
+
+	return 0;
+}

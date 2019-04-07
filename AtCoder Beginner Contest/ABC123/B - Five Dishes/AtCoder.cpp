a069cc8274e34787236533c4941310fcf6e5321a
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-07 19:36:24 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC123/B - Five Dishes/AtCoder.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+const int inf = 1012345678;
+
+int main() {
+	vector<int> A(5);
+	cin >> A[0] >> A[1] >> A[2] >> A[3] >> A[4];//注文から届く時間
+	vector<int> P = { 0, 1, 2, 3, 4 };
+	int final_answer = inf; //最小の時間
+	do {
+		int ans = 0;	//かかる時間の合計
+		for (int i = 0; i < 5; ++i) {
+			while (ans % 10 != 0) { 
+				ans += 1; 
+			}
+			//P[i]は変化したパターンの数字
+			ans += A[P[i]];
+		}
+		final_answer = min(final_answer, ans);
+	} while (next_permutation(P.begin(), P.end()));
+	//next_permutation：順列生成 全てのパターンを生成する
+	cout << final_answer << endl;
+	return 0;
+}
---
+++ AtCoder Beginner Contest/ABC123/B - Five Dishes/AtCoder2.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	int N = 5;
+
+	//注文してから届く時間
+	vector<int> Time = { a,b,c,d,e };//入力した数字が入る場所
+	//注文してから次に注文できるまでの時間
+	vector<int> nextTime(N);//10の倍数にした時間が入る場所
+
+	//並べ替え
+	for (int i = 0; i < N; i++) {
+		if (Time[i] % 10 == 0) {
+			nextTime[i] = Time[i];
+		}
+		else {
+			//一の位を切り捨て10の倍数にする
+			nextTime[i] = Time[i] - Time[i] % 10 + 10;
+		}
+	}
+
+	int BestTime = 999999999;//最小の時間
+	for (int i = 0; i < N;i++) {
+		int SumeTime = 0;//合計
+		for (int j = 0; j < N; j++) {
+			if (i==j) {
+				//最後の処理
+				SumeTime += Time[j];
+			}
+			else {
+				//最後以外の処理
+				SumeTime += nextTime[j];
+			}
+		}
+		BestTime = min(BestTime, SumeTime);
+	}
+	cout << BestTime << endl;
+	return 0;
+}

ed0f0e9d7a659e60209b8c3b75bd7ca6dbb40582
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-08 22:00:11 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC123/C - Five Transportations/AtCoder.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	double N;
+	cin >> N;
+	double A;
+	double minimum = 10000000000000010LL;
+	for (int i = 0; i < 5; i++) {
+		cin >> A;
+		minimum = min(minimum, A);
+	}
+
+	cout << (long long)ceil(N / minimum) + 4 << endl;
+}

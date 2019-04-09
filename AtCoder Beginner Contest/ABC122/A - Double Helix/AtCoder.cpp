91ba751a164458cd81007b1c62688fc3678f83dc
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-09 20:52:09 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC122/A - Double Helix/AtCoder.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+
+	string s,t;
+	cin >> s;
+	if (s == "A") {t = "T";}
+	if (s == "T") {t = "A";}
+	if (s == "C") {t = "G";}
+	if (s == "G") {t = "C";}
+	cout << t << endl;
+}

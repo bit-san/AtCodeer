1b96dd2d8351ae6e2ef75a634b47d71cdb88ddcc
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-09 21:10:10 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC122/B - ATCoder/AtCoder.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int a = 0;
+	int maxsss = 0;
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size();i++) {
+		if (s.at(i)=='A'|| s.at(i) == 'C'|| s.at(i) == 'G'|| s.at(i) == 'T') {a++;}
+		else {a = 0;}
+		maxsss = max(maxsss, a);
+	}
+
+	cout << maxsss << endl;
+}

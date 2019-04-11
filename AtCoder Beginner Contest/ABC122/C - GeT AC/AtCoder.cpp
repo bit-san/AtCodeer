d3bae04c71de82ec0fdf5e0035039cd15a16afcd
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-11 18:44:01 +0900

    Update AtCoder.cpp

--- AtCoder Beginner Contest/ABC122/C - GeT AC/AtCoder.cpp
+++ AtCoder Beginner Contest/ABC122/C - GeT AC/AtCoder.cpp
@@ -7,16 +7,15 @@ int n, q, l, r;
 string s;
 int main() {
 	cin >> n >> q >> s;
-	//文字列は1~Nと数える
 	//ACACTACG
 	//01234567 : s
 	
 	vector<int> a(n+1);
 
 	for (int i = 1; i < n; i++) {
-		//-ACACTACG
-		//012345678: a
-		//--1-2--3-
+		//ACACTACG
+		//01234567 : s
+		//00112223 : a
 		a.at(i+1) = a.at(i);
 		if (s.at(i - 1) == 'A'&&s.at(i) == 'C') {
 			a.at(i + 1) += 1;

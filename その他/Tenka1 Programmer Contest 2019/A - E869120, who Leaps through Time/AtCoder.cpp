8cbe9741277b2aadafa38f3a9c75e5ea638e014c
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-14 22:48:19 +0900

    Add files via upload

---
+++ その他/Tenka1 Programmer Contest 2019/A - E869120, who Leaps through Time/AtCoder.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int N, T, res = 0, time = 0;
+	cin >> N >> T;
+	vector<int> A(N);
+	for (int i = 1; i < N; i++) {
+		cin >> A.at(i);
+	}
+	for (int j = 1; j < N; j++) {
+		time = time + A.at(j);
+	}
+	while (time > 0) {
+		time = time - T;
+		res++;
+	}
+	cout << res << endl;
+}
---
+++ その他/Tenka1 Programmer Contest 2019/C - Infinite Grid/AtCoder.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+int main() {
+	int H, W;
+	cin >> H >> W;
+	vector<vector<char>> str(H, vector<char>(W));
+
+	for (int x = 0; x < H; x++) {
+		for (int y = 0; y < W; y++) {
+			cin >> str.at(x).at(y);
+		}
+	}
+
+	int i = 0, j = 0;
+	bool a = false, b = false;
+	while (1) {
+		if (j + 1 < W) {
+			if (str.at(i).at(j + 1) == '.') {
+				j++;
+				a = true;
+			}
+			else if (str.at(i).at(j + 1) == '#') {
+				j;
+				a = false;
+			}
+		}
+		if (i == H - 1 && j == W - 1) {
+			cout << "Yay!" << endl;
+			break;
+		}
+		else if (a == false) {
+			cout << ":(" << endl;
+			break;
+		}
+
+	}
+}
+

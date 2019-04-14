ce06e82c2a2893c0e8c1e80c955af26bd5e8e76f
bit-san <48820371+bit-san@users.noreply.github.com>
2019-04-14 23:04:39 +0900

    Revert "Revert "Add files via upload""
    This reverts commit bb9847df24d997df9f3ad0c817d691958fb988e3.

---
+++ AtCoder Beginner Contest/ABC124/A - Buttons/AtCoder.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+int main(void) {
+	cin.tie(0); ios::sync_with_stdio(false);
+	int a, b, MAXX = 0;
+	cin >> a >> b;
+	for (int i = 0; i < 2; i++) {
+		if (a > b) {
+			MAXX = MAXX + a;
+			a--;
+		}
+		else if (b > a) {
+			MAXX = MAXX + b;
+			b--;
+		}
+		else {
+			MAXX = MAXX + a;
+			a--;
+		}
+	}
+	cout << MAXX << endl;
+}

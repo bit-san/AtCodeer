71794190c1b99ecede3231d2c4e588d7d9ad4543
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-15 21:13:23 +0900

    Add files via upload

---
+++ その他/square869120Contest #6/B - AtCoder Market/AtCoder.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+long long N, A[100], B[100], minx = 1e11;
+
+//全探索
+long long solve(int p1, int p2) {
+	long long V1 = 0;
+	for (int i = 1; i <= N; i++) {
+		//絶対値を求める
+		V1 += abs(p1 - A[i]);
+		V1 += abs(A[i] - B[i]);
+		V1 += abs(B[i] - p2);
+	}
+	return V1;
+}
+
+int main() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i] >> B[i];
+	}
+	cout << minx << endl;
+	vector<long long>E;
+	for (int i = 1; i <= N; i++) {
+		E.push_back(A[i]);
+	}
+	for (int i = 1; i <= N; i++) {
+		E.push_back(B[i]);
+	}
+
+	//拡張for文
+	for (long long v1 : E) {
+		for (long long v2 : E) {
+			minx = min(minx, solve(v1, v2));
+		}
+	}
+	cout << minx << endl;
+	return 0;
+}
+
+

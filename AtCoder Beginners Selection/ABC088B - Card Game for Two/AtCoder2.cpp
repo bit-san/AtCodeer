602e16eb0652d3a4fa6aa123893c06c0550a26d0
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-18 20:00:48 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC088B - Card Game for Two/AtCoder2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+#define F(i,N) for (int i = 0; i < n; i++)
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+	int n;cin >> n;
+	vector<int> arr(n);
+	F(i, n) cin >> arr.at(i);
+	sort(arr.begin(),arr.end());
+	int a = 0, b = 0;
+	F(i, N) {
+		if (i % 2 == 0) a += arr.at(i);
+		else b += arr.at(i);
+	}
+	cout << abs(a - b) << "\n";
+	return 0;
+}

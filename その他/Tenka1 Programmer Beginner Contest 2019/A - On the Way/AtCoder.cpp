ad19685a8b2ad98774fd9b9de9286daf9eb71c0b
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-20 22:44:38 +0900

    Add files via upload

---
+++ その他/Tenka1 Programmer Beginner Contest 2019/A - On the Way/AtCoder.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+	int A, B, C;
+	cin >> A >> B >> C;
+	if ((A <= C && C <= B) || (A >= C && B <= C)) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+}
---
+++ その他/Tenka1 Programmer Beginner Contest 2019/B - eeeee/AtCoder.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+	int N, K;
+	cin >> N;
+	char S[100];
+	for (int i = 0; i < N; i++) { cin >> S[i]; }
+	cin >> K;
+	for (int i = 0; i < N; i++)
+	{
+		if (S[i] != S[K - 1]) { S[i] = '*'; }
+	}
+	for (int i = 0; i < N; i++) { cout << S[i]; }
+}

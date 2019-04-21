71cefe71257375b7ca82455c0c7b66451e6e7f3d
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-21 19:44:40 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC049C - 白昼夢  Daydream/AtCoder2.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string S;cin >> S;
+
+	if (regex_match(S, regex("(dream(er)?|eraser?)*"))) {
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+}

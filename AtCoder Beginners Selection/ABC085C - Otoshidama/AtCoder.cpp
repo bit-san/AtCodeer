517b4acd36614e09ab7de2406769f801a9655656
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-21 17:19:42 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC085C - Otoshidama/AtCoder.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+	
+	int N, Y;
+	cin >> N >> Y;
+
+	for (int i = 0; i <= N; i++)
+	{
+		for (int j = 0; j <= N; j++)
+		{
+			for (int k = 0; k <= N; k++)
+			{
+				if ((N==i+j+k)&&(Y == (i*10000+j*5000+k*1000))) {
+					cout << i << " " << j << " " << k << endl;
+					return 0;
+				}
+			}
+		}
+	}
+
+	cout << -1 << " " << -1 << " " << -1 << endl;
+
+	
+}
---
+++ AtCoder Beginners Selection/ABC085C - Otoshidama/AtCoder2.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+//TLE
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, Y, Z;
+	cin >> N >> Y;
+
+	for (int i = 0; i <= N; i++)
+	{
+		for (int j = 0; j <= N; j++)
+		{
+			Z = N - i - j;
+			if ((Z >= 0) && (Y == (i * 10000 + j * 5000 + Z * 1000))) {
+				cout << i << " " << j << " " << Z << endl;
+				return 0;
+			}
+		}
+	}
+
+	cout << -1 << " " << -1 << " " << -1 << endl;
+
+
+}

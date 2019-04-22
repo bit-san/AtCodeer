14166a8fbc45b0658482f885a47d08bbcbf548fb
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-22 21:10:04 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC086C - Traveling/AtCoder.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int n, t, x, y;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> t >> x >> y;
+		if (x + y > t || (x + y + t) % 2 != 0) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+}

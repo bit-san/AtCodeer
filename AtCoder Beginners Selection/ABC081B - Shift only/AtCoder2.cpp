a392e3867031a2daee956482f76b06eaac12e0b5
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-23 20:14:31 +0900

    Add files via upload

---
+++ AtCoder Beginners Selection/ABC081B - Shift only/AtCoder2.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, A[200], cunt = 0;
+	cin >> N;
+	
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	
+	while (1) {
+		for (int i = 0; i < N; i++)
+		{
+			if (A[i] % 2 != 0) {
+				cout << cunt << endl;
+				return 0;
+			}
+			else {
+				A[i] /= 2;
+			}
+		}
+		cunt++;
+	}
+}

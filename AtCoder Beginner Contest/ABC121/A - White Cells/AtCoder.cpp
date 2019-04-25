ab45f416e147c79d9aed4f7635200e151608fa42
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-25 20:15:46 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC121/A - White Cells/AtCoder.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int H, W, h, w;
+	cin >> H >> W >> h >> w;
+
+	cout << (H - h) * (W - w) << endl;
+}

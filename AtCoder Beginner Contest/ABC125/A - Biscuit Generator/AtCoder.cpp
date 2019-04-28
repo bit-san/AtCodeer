bbb5c741d3d9b3bc4279e46dfe5f56c1a16f99d2
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-28 22:08:28 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC125/A - Biscuit Generator/AtCoder.cpp
@@ -0,0 +1,9 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+int main() {
+	int A, B, T;
+	double N;
+	cin >> A >> B >> T;
+	N = T + 0.5;
+	cout << (int)(N / A) *B << endl;
+}
---
+++ AtCoder Beginner Contest/ABC125/B - Resale/AtCoder.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h" 
+using namespace std;
+int main() {
+	int N, res, S = 0;
+	cin >> N;
+
+	vector<int> V(N);
+	vector<int> C(N);
+	vector<int> X(N);
+
+	for (int i = 0; i < N; i++) {cin >> V.at(i);}
+	for (int i = 0; i < N; i++) {cin >> C.at(i);}
+	for (int i = 0; i < N; i++) {
+		res = V.at(i) - C.at(i);
+		if (res < 0) X.at(i) = 0;
+		else X.at(i) = res;
+	}
+	for (int i = 0; i < N; i++) {S = S + X.at(i);}
+	cout << S << endl;
+}

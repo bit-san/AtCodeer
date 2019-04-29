6719a9889a0174657942fe558ab57cf6e0cbeffe
Tsune <48820371+bit-san@users.noreply.github.com>
2019-04-29 21:40:04 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC125/D - Flipping Signs/AtCoder.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+	int N, cunt = 0;
+	long long res = 0, mini = 9999999999;
+	cin >> N;
+	vector<long long> A(N);
+
+	for (int i = 0; i < N; i++){
+		cin >> A.at(i);
+		if (A.at(i)<0) {
+			cunt++;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		mini = min(mini, abs(A.at(i)));
+		res += abs(A.at(i));
+	}
+
+	if (cunt % 2 == 0) {
+		cout << res << endl;
+		return 0;
+	}
+	else {
+		res = res - 2 * mini;
+		cout << res << endl;
+	}
+}

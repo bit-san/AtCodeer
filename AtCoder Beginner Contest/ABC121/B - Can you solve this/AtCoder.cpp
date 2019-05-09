c83dba0fac040ad82bcbd18cc34443312eb227fb
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-09 20:29:36 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC121/B - Can you solve this/AtCoder.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+using namespace std;
+int main() {
+	int N, M, C, cunt = 0;
+	cin >> N >> M >> C;
+	vector<int> B(M);
+	vector<int> A(M);
+	for (int i = 0; i < M; i++)	{cin >> B.at(i);}
+
+	for (int i = 0; i < N; i++) { 
+		int res = 0;
+		for (int x = 0; x < M; x++) { cin >> A.at(x); }
+		for (int y = 0; y < M; y++) { 
+			res = res + A.at(y)*B.at(y);
+		}
+		if (res+C >0) {
+			cunt++;
+		}
+	}
+	cout << cunt << endl;
+}

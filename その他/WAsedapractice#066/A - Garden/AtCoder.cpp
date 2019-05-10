176a005e87e765946f6ed812838c0a8623154e28
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-10 23:17:52 +0900

    Add files via upload

---
+++ その他/WAsedapractice#066/A - Garden/AtCoder.cpp
@@ -0,0 +1,7 @@
+﻿#include "bits/stdc++.h"
+using namespace std;
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << (A - 1)*(B - 1) << endl;
+}
---
+++ その他/WAsedapractice#066/B - 105/AtCoder.cpp
@@ -0,0 +1,20 @@
+﻿#include "bits/stdc++.h"
+
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+using namespace std;
+int main() {
+	int N, ans = 0, tmp = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i += 2) {
+		tmp = 0;
+		for (int j = 1; j <= i; j++) {
+			if (i%j == 0) { tmp++; }
+		}
+		if (tmp == 8) { ans++; }
+	}
+	cout << ans << endl;
+}

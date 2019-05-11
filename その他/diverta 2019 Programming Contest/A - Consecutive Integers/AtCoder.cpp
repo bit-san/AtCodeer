75d9f4f3291384603dc3e84aefba21599a0a7885
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-11 23:20:29 +0900

    Add files via upload

---
+++ その他/diverta 2019 Programming Contest/A - Consecutive Integers/AtCoder.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+#define rep(i, n) for(int i = 1; i < (int)(n); i++)
+
+using namespace std;
+int main() {
+	int N, K, res;
+	cin >> N >> K;
+	res = N - K + 1;
+	cout << res << endl;
+}
---
+++ その他/diverta 2019 Programming Contest/B - RGB Boxes/AtCoder.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b < a) { a = b; return 1; } return 0; }
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+
+using namespace std;
+int main() {
+	int R, G, B, N, sum, res = 0;
+	cin >> R >> G >> B >> N;
+
+	for (int i = 0; i <= N; i++)
+	{
+		for (int j = 0; j <= N; j++)
+		{
+			sum = N - (i*R) - (j*G);
+			if (sum >= 0 && sum%B == 0) {
+				res++;
+			}
+		}
+	}
+	cout << res << endl;
+}

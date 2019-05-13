02e12a17bd195d013752e8577d00efed6bd34fd6
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-13 22:14:46 +0900

    Add files via upload

---
+++ その他/diverta 2019 Programming Contest/D - DivRem Number/AtCoder.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	ll N, k, m, res = 0;
+	cin >> N;
+	for (ll i = 1; i < sqrt(N)+1 ; i++)
+	{
+		if (N % i == 0) {
+			m = (N / i) - 1;
+			if (m > i) { res += m; }
+		}
+	}
+	cout << res << endl;
+}

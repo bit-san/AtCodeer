f7f5195ee8914c7d031feed47c0b3962d6b9834e
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-14 20:34:59 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC120/A - Favorite Sound/AtCoder.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int A, B, C, res;
+	cin >> A >> B >> C;
+	if (B / A < C) {
+		res = B / A;
+	}
+	else {
+		res = C;
+	}
+	cout << res << endl;
+}
---
+++ AtCoder Beginner Contest/ABC120/B - K-th Common Divisor/AtCoder.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int A, B, K, res;
+	cin >> A >> B >> K;
+	for (int i = min(A,B); i >= 1 ; i--)
+	{
+		if (A%i==0 && B%i==0) {
+			K--;
+			if (K==0) {
+				cout << i << endl;
+			}
+		}
+	}
+}

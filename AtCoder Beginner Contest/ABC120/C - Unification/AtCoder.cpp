f9bd202848d495bdb5b85e3e0c94a4b4825302bd
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-17 22:52:55 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC120/C - Unification/AtCoder.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int N,res=0,A=0,B=0;
+	string S;
+	cin >> S;
+	for (int i = 0; i < S.size(); i++)
+	{
+		if (S.at(i) == '1') { A++; }
+		if (S.at(i) == '0') { B++; }
+	}
+	res = 2 * min(A, B);
+	cout << res << endl;
+}

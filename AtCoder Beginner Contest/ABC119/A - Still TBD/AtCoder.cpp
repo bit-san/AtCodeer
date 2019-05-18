b8633e4b77db10b605891bf6bb277a7087d66e8c
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-18 21:38:43 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC119/A - Still TBD/AtCoder.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	string S;
+	cin >> S;
+	if (S[6] > '4' || S[5] == '1') {
+		cout << "TBD" << endl;
+	}
+	else {
+		cout << "Heisei" << endl;
+	}
+}
---
+++ AtCoder Beginner Contest/ABC119/B - Digital Gifts/AtCoder.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int N;
+	double x,res = 0;
+	string S;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x >> S;
+		if (S == "JPY") {
+			res = res + x;
+		}
+		else if (S == "BTC") {
+			res = res + (double)(x)* 380000.0;
+		}
+	}
+	cout << res << endl;
+}

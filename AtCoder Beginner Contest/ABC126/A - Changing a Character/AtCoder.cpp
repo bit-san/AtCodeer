ab40820122632e612ebfc902bf8d845c8c954852
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-19 22:47:53 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC126/A - Changing a Character/AtCoder.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int N, K;
+	string S;
+	cin >> N >> K >> S;
+	K = K - 1;
+	if (S[K] == 'A') {
+		S[K] = 'a';
+	}
+	else if (S[K] == 'B') {
+		S[K] = 'b';
+	}
+	else if (S[K] == 'C') {
+		S[K] = 'c';
+	}
+	cout << S << endl;
+}
---
+++ AtCoder Beginner Contest/ABC126/B - YYMM or MMYY/AtCoder.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+int main() {
+	int S, Y, M;
+	cin >> S;
+
+	Y = S / 100;
+	M = S % 100;
+	//cout << Y << " " << M << endl;
+
+	if (Y > 12 && M > 12 || Y > 12 && M == 0 || Y == 0 && M > 12 || Y == 0 && M == 0) {
+		cout << "NA" << endl;
+	}
+	else if (M > 0 && Y > 0 && M < 13 && Y < 13) {
+		cout << "AMBIGUOUS" << endl;
+	}
+	else if (M < 13 && 0 < M) {
+		cout << "YYMM" << endl;
+	}
+	else if (Y < 13 && 0 < Y) {
+		cout << "MMYY" << endl;
+	}
+}

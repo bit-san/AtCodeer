9f2e7723584bcdb7df66f5aaf5e0a0b2333f067a
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-20 00:22:28 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC126/C - Dice and Coin/AtCoder.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	long double N, K, res = 0.0, A, ten;
+	cin >> N >> K;
+	A = 1.0 / N;
+	for (int i = 1; i <= N; i++)
+	{
+		double cunt = 0;
+		ten = (double)i;
+		while (ten < K)
+		{
+			ten = ten * 2.0;
+			cunt++;
+		}
+		res = res + A * pow(1.0 / 2.0, cunt);
+	}
+	cout << setprecision(12) << res << endl;
+}

39368d09cf7a05a387ef9b4944a4ac92d482bc5a
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-22 22:44:12 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC118/A - B +- A/AtCoder.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	if ( B%A == 0) {
+		cout << A + B << endl;
+	}
+	else {
+		cout << B - A << endl;
+	}
+}

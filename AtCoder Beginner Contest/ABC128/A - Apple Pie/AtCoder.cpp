0919093ffbac629c764ac1ce3c6cd83f641cadae
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-26 22:41:42 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC128/A - Apple Pie/AtCoder.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	int A, P, res;
+	cin >> A >> P;
+	res = (A * 3 + P) / 2;
+	cout << res << endl;
+}

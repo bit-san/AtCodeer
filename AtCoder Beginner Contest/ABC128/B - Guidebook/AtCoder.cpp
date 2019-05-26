b81d2528926b57805852d1f362a46b7d63d1070e
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-26 22:52:36 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC128/B - Guidebook/AtCoder.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	int N, p, x = 1;
+	cin >> N;
+	vector<pair<string, int>> r(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> r[i].first >> p;
+		r[i].second = -10000 * p - x; //何番目の入力か
+		x++;
+	}
+
+	sort(r.begin(), r.end());
+	/*
+	for (int i = 0; i < N; i++) {
+		cout << r[i].first << ":" << r[i].second << endl;
+	}
+	*/
+
+	for (int i = 0; i < N; i++) {
+		cout << (-1 * r[i].second) % 1000 << endl;
+	}
+}

d5a5c33b88a3f9568ac6eb240a090772d1826410
Tsune <48820371+bit-san@users.noreply.github.com>
2019-05-30 20:02:55 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC128/B - Guidebook/AtCoder2.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	tuple<string,int,int> p[100];
+
+	int A, T; cin >> A;
+	string S;
+	for (int i = 0; i < A; i++) {
+		cin >> S >> T;
+		p[i] = make_tuple(S, -T, i);
+	}
+	sort(p, p + A);
+	for (int i = 0; i < A; i++) {
+		int a = get<2>(p[i]);
+		cout << a + 1 << endl;
+	}
+}

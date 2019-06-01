f06e7544bb17ba75d2e02fc95b0ed4446ce4dd06
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-01 23:08:11 +0900

    Add files via upload

---
+++ その他/M-SOLUTIONS プロコンオープン/B - Sumo/AtCoder.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long int lli;
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string S; cin >> S;
+	int n = 0;
+	for (int i = 0; i < S.size(); i++)
+	{
+
+		if (S[i] == 'o') { n++; }
+	}
+	if ((n + 15 - S.size()) >= 8) { cout << "YES" << endl; }
+	else { cout << "NO" << endl; }
+}

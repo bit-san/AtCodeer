e779142c3acf2d418e055087d70e9789fdad0fa4
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-05 20:25:59 +0900

    Add files via upload

---
+++ AtCoder Grand Contest/AGC034/B - ABC/AtCoder.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long int lli;
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string s;
+	cin >> s;
+	lli res = 0, cur = 0;
+	for (int i = 0; i < s.size()-1; i++) {
+		if (s[i] == 'A') { cur++; }
+		else {
+			if (s[i] == 'B' && s[i + 1] == 'C') {
+				res += cur;
+				i++;
+			}
+			else { cur = 0; }
+		}
+	}
+	cout << res << endl;
+}

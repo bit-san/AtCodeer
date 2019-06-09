1621351875657cb62c881339f9da0076de8dc4b0
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-09 22:18:59 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC129/A - Airplane/AtCoder.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long int lli;
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	vector<int> N(3);
+	int res;
+	cin >> N[0] >> N[1] >> N[2];
+	sort(N.begin(), N.end());
+	cout << N[0] + N[1] << endl;
+}
---
+++ AtCoder Beginner Contest/ABC129/B - Balance/AtCoder.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long int lli;
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, cunt = 1, res = 999999999;
+	cin >> N;
+	vector<int> ver(N);
+
+	for (int i = 0; i < N; i++) { cin >> ver[i]; }
+
+	while (cunt < N) {
+		int A = 0, B = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (i > cunt) { A += ver[i]; }
+			else { B += ver[i]; }
+		}
+		res = min(res, abs(A - B));
+		cunt++;
+	}
+	cout << res << endl;
+}

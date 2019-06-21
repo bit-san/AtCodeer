8fa3e4fe6538266c224e2f9cc1020779e1dab249
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-21 23:20:19 +0900

    Add files via upload

---
+++ その他/diverta 2019 Programming Contest 2/B - Picking Up/AtCoder.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int N; vector<int> X, Y;
+int solve() {
+
+	int res = N;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			if (i == j) continue;
+			long long dx = X[j] - X[i], dy = Y[j] - Y[i];
+
+			int sub = 0;
+			for (int i2 = 0; i2 < N; ++i2) {
+				for (int j2 = 0; j2 < N; ++j2) {
+					if (j2 == i2) continue;
+					if (dx == X[j2] - X[i2] && dy == Y[j2] - Y[i2]) ++sub;
+				}
+			}
+			res = min(res, N - sub);
+		}
+	}
+	return res;
+}
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	cin >> N;
+	X.resize(N); Y.resize(N);
+
+	if (N == 1) { cout << "1" << endl; return 0; }
+	rep(i, N) { cin >> X[i] >> Y[i]; }
+
+	cout << solve() << endl;
+}

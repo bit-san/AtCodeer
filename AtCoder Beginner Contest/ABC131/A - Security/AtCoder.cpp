af52ff6d9a7d612de0d5e063f317f969f83514bf
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-22 23:46:27 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC131/A - Security/AtCoder.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string N;
+	cin >> N;
+	for (int i = 0; i < 3; i++) {
+		if (N[i] == N[i + 1]) {
+			cout << "Bad" << endl;
+			return 0;
+		}
+	}
+	cout << "Good" << endl;
+}
---
+++ AtCoder Beginner Contest/ABC131/B - Bite Eating/AtCoder.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, L, sum = 0; cin >> N >> L;
+	vector<int> ver(N + 1, 0);
+	for (int i = 1; i < N + 1; i++) {
+		ver[i] = L + i - 1;
+		sum += ver[i];
+	}
+	for (int i = 1; i < N + 1; i++) {
+		if (ver[i] == 0) {
+			cout << sum << endl;
+			return 0;
+		}
+	}
+	int A = -99999, B = 99999;
+	if (sum >= 0) {
+		for (int i = 1; i < N + 1; i++) {
+			A = max(A, sum - ver[i]);
+		}
+		cout << A << endl;
+		return 0;
+	}
+	else {
+		for (int i = 1; i < N + 1; i++) {
+			B = min(B, sum - ver[i]);
+		}
+		cout << B << endl;
+		return 0;
+	}
+}
+}

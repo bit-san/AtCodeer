f7dad5707c4edf436e0699f02c2aad437c99e353
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-24 21:41:33 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC131/B - Bite Eating/AtCoder2.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < n; i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, A, eat; cin >> N >> A;
+	
+	int R = A + N - 1;
+	int L = A;
+
+	if (R <= 0) { eat = R; }
+	else if (L >= 0) { eat = L; }
+	else { eat = 0; }
+
+	cout << (R+L)*(R-L+1) / 2 - eat << endl;
+}

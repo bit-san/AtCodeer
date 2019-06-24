9f4e8a576a814f763bad8bc4e00bdedd1200fc91
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-24 21:53:01 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC131/C - Anti-Division/AtCoder.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+lli GCD(ll a, ll b) {return b ? GCD(b, a % b) : a;}
+lli LCM(ll a, ll b) { return a / GCD(a, b) * b; }
+
+lli f(lli x, lli a, lli b) {
+	lli res = x;
+	res -= x / a;
+	res -= x / b;
+	res += x / LCM(a, b);
+	return res;
+}
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	lli A, B, C, D; cin >> A >> B >> C >> D;
+	
+	lli ans = f(B, C, D) - f(A - 1, C, D);
+	cout << ans << endl;
+
+}

ed15680df8f6df6f4e460a0ce26753ae53cb93dd
Tsune <48820371+bit-san@users.noreply.github.com>
2019-06-29 23:40:20 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC132/A - Fifty-Fifty/AtCoder.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string S; cin >> S;
+
+	if (S[0] == S[1] && S[1] == S[2] && S[2] == S[3]) {
+		cout << "No" << endl;
+		return 0;
+	}
+	if ((S[0] == S[1] && S[2] == S[3]) ||
+		(S[0] == S[3] && S[1] == S[2]) ||
+		(S[0] == S[2] && S[1] == S[3])) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+}
---
+++ AtCoder Beginner Contest/ABC132/B - Ordinary Number/AtCoder.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, res = 0; cin >> N;
+	vector<int>ver(N);
+	rep(i, N) { cin >> ver[i]; }
+
+	for (int i = 1; i < N - 1; i++) {
+		if (ver[i - 1] > ver[i] && ver[i] > ver[i + 1]) {
+			res++;
+		}
+		else if (ver[i - 1] < ver[i] && ver[i] < ver[i + 1]) {
+			res++;
+		}
+	}
+	cout << res << endl;
+}

18ef58722e9e17e6aa072ca49e6245f176a69c57
Tsune <48820371+bit-san@users.noreply.github.com>
2019-07-02 18:11:26 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC132/C - Divide the Problems/AtCoder.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, res; cin >> N;
+	vector<int>ver(N);
+	rep(i, N) { cin >> ver[i]; }
+	sort(ver.begin(), ver.end());
+	res = ver[N / 2] - ver[N / 2 - 1];
+	cout << res << endl;
+}

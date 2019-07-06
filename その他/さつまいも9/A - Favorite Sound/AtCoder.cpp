d37a14d8d9c42daf93ab75a4b7ca14f90c75a4b2
Tsune <48820371+bit-san@users.noreply.github.com>
2019-07-06 21:34:17 +0900

    Add files via upload

---
+++ その他/さつまいも9/A - Favorite Sound/AtCoder.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int A, B, C, res; cin >> A >> B >> C;
+
+	res = B / A;
+	res = res > C ? C : res;
+	cout << res << endl;
+}
---
+++ その他/さつまいも9/B - KEYENCE String/AtCoder.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	string S; cin >> S;
+
+	for (int i = 0; i < S.size(); i++) {
+		for (int j = i; j < S.size(); j++) {
+			string A = S;
+			A = A.substr(0, i) + A.substr(j, S.size() - j);
+			if (A == "keyence") {
+				cout << "YES" << endl;
+				return 0;
+			}
+		}
+	}
+	cout << "NO" << endl;
+	return 0;
+}

6f563ccca8330638413c92076866da37d89b8105
Tsune <48820371+bit-san@users.noreply.github.com>
2019-07-07 22:02:40 +0900

    Add files via upload

---
+++ AtCoder Beginner Contest/ABC133/A - T or T/AtCoder.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, A, B; cin >> N >> A >> B;
+
+	if (N*A < B) { cout << N * A << endl; }
+	else { cout << B << endl; }
+}
---
+++ AtCoder Beginner Contest/ABC133/B - Good Distance/AtCoder.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "bits/stdc++.h"
+
+typedef long long ll;
+typedef long long int lli;
+const int MOD = 1000000007;
+#define rep(i,n) for (int i = 0; i < (n); i++)
+
+using namespace std;
+
+int main() {
+	cin.tie(0); ios::sync_with_stdio(false);
+
+	int N, D; cin >> N >> D;
+	vector< vector<int> > ver(N, vector<int>(D));
+	rep(i, N) {
+		rep(j, D) { cin >> ver[i][j]; }
+	}
+
+	int res = 0;
+	rep(x, N) {
+		for (int y = x + 1; y < N; y++) {
+			double sum = 0;
+			rep(z, D) { sum += pow(ver[x][z] - ver[y][z], 2); }
+			sum = sqrt(sum);
+			if (sum == (int)sum) { res++; }
+		}
+	}
+	cout << res << endl;
+}
